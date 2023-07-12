f4350986ba2e61d31ec0148191a659767bee5ea5
lzheng3 <lekaizheng@icloud.com>
2023-07-11 22:50:33 -0400

    create people's screen

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,19 @@
+import UserList from "@/components/UserList";
 import Sidebar from "@/components/sidebar/Sidebar";
+import getUsers from "@/utils/getUsers";
 
 export default async function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
